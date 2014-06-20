62ac0f9c966dbb07dc2492308ded4c904dc4e330
raspberrypi624 <raspberrypi624@gmail.com>
2014-06-20 11:34:10 +0000

    initial

---
+++ proxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export http_proxy=http://2011189:Sushilneeru@172.27.16.154:3128/
+export https_proxy=http://2011189:Sushilneeru@172.27.16.154:3128/
+export ftp_proxy=http://2011189:Sushilneeru@172.27.16.154:3128/
